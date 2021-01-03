2d62327785d865da07b377669a60eb6358113664
Iliyas Jorio <iliyas@jor.io>
2021-01-03 16:40:40 +0100

    Nuke internet

--- src/Headers/internet.h
+++
@@ -1,9 +0,0 @@
-//
-// internet.h
-// 
-
-void ReadHTTPData_VersionInfo(void);
-OSStatus LaunchURL(ConstStr255Param urlStr);
-Boolean IsInternetAvailable(void);
-OSStatus DownloadURL(const char *urlString);
-
