0376cf2b215c11b81ba06a65f6727cabac69ece7
Iliyas Jorio <iliyas@jor.io>
2021-01-03 22:49:03 +0100

    Fix unresolved symbols related to SkeletonAnim.c - IT LINKS!!

--- src/Skeleton/SkeletonAnim.c
+++ src/Skeleton/SkeletonAnim.c
@@ -22,7 +22,7 @@ static void InterpolateKeyFrames(const JointKeyframeType *kf1, const JointKeyfra
 static void GetModelMorphPosition(const SkeletonObjDataType *skeleton,long jointNum, JointKeyframeType *interpKf);
 static short GetNextAnimEventAtTime(const SkeletonObjDataType *skeleton, float time);
 static float CalcMaxKeyFrameTime(const SkeletonObjDataType *skeleton);
-inline float	AccelerationPercent(float percent);
+inline static float	AccelerationPercent(float percent);
 static void SetSkeletonAnimGuts(SkeletonObjDataType *skeleton, long animNum);
 
 
@@ -679,7 +679,7 @@ float	x,n;
 
 /******************** ACCELERATION PERCENT ***************************/
 
-inline float	AccelerationPercent(float percent)
+inline static float	AccelerationPercent(float percent)
 {
 long	i;
 
